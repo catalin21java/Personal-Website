96d8e27d62c465e594c138ee53a889f61efd8db4
Catalin <catalinborzei@yahoo.com>
2023-11-14 19:51:57 +0200

    update

---
+++ website-main/personal website/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".auto-input",{
+
+    strings:["Catalin Borzei!","Front-End enthusiast!","Java enthusiast!","UI Designer!","Robotics enthusiast!","UE5 fan!"],
+    typeSpeed:100,
+    backSoeed:100,
+    loop:true
+    
+    });
+
